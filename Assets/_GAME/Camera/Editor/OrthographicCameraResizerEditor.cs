05527bf1d973d37893a5739e68c5276c87a0215b
Hubert GROSSIN <hubert.grossin@gmail.com>
2020-09-27 15:27:26 +0200

    Add CameraResizer

---
+++ Assets/_GAME/Camera/Editor/OrthographicCameraResizerEditor.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Game
+{
+
+    ///<summary>
+    /// 
+    ///</summary>
+    [CustomEditor(typeof(OrthographicCameraResizer))]
+    public class OrthographicCameraResizerEditor : Editor
+	{
+
+        private const string CAMERA_PROP = "m_Camera";
+        private const string RATIO_PROP = "m_Ratio";
+        private const string SIZE_PROP = "m_Size";
+
+        private SerializedProperty m_CameraProp = null;
+        private SerializedProperty m_RatioProp = null;
+        private SerializedProperty m_SizeProp = null;
+
+        private OrthographicCameraResizer m_CameraResizer = null;
+
+        private void OnEnable()
+        {
+            m_CameraProp = serializedObject.FindProperty(CAMERA_PROP);
+            m_RatioProp = serializedObject.FindProperty(RATIO_PROP);
+            m_SizeProp = serializedObject.FindProperty(SIZE_PROP);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if(CameraResizer.Camera != null && !CameraResizer.Camera.orthographic)
+            {
+                EditorGUILayout.HelpBox("The target camera's projection must be set to \"Orthographic\"", MessageType.Warning);
+            }
+
+            EditorGUILayout.PropertyField(m_CameraProp);
+
+            Vector2 ratio = EditorGUILayout.Vector2Field(m_RatioProp.displayName, m_RatioProp.vector2Value);
+            if (ratio != m_RatioProp.vector2Value)
+            {
+                CameraResizer.Ratio = ratio;
+            }
+
+            Vector2 size = EditorGUILayout.Vector2Field(m_SizeProp.displayName, m_SizeProp.vector2Value);
+            if (size != m_SizeProp.vector2Value)
+            {
+                CameraResizer.Size = size;
+            }
+
+            CameraResizer.ResizeCamera();
+        }
+
+        public OrthographicCameraResizer CameraResizer
+        {
+            get
+            {
+                if (m_CameraResizer == null)
+                {
+                    m_CameraResizer = target as OrthographicCameraResizer;
+                }
+                return m_CameraResizer;
+            }
+        }
+
+    }
+
+}
---
+++ Assets/_GAME/Camera/OrthographicCameraResizer.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+
+    ///<summary>
+    /// Resizes an orthographic camera depending on the number of cells you want on X or Y.
+    ///</summary>
+    [AddComponentMenu("_GAME/Camera Resizer")]
+    public class OrthographicCameraResizer : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Camera m_Camera = null;
+
+        [SerializeField]
+        private Vector2 m_Ratio = new Vector2(16f, 9f);
+
+        [SerializeField]
+        private Vector2 m_Size = new Vector2(0f, 5f);
+
+        /// <summary>
+        /// Ensure the current size has been applied to the Camera's orthographic size.
+        /// </summary>
+        public void ResizeCamera()
+        {
+            if (Camera != null)
+            {
+                Camera.orthographicSize = m_Size.y / 2;
+            }
+        }
+
+        /// <summary>
+        /// Gets/Sets the screen ratio.
+        /// </summary>
+        public Vector2 Ratio
+        {
+            get { return m_Ratio; }
+            set
+            {
+                // If the X component of the ratio has changed
+                if(m_Ratio.x != value.x)
+                {
+                    float sizeRatio = m_Size.x / m_Ratio.x;
+                    float ratioDiff = value.x - m_Ratio.x;
+                    float finalSize = m_Size.x + sizeRatio * ratioDiff;
+                    Size = new Vector2(finalSize, m_Size.y);
+                }
+                else
+                {
+                    float sizeRatio = m_Size.y / m_Ratio.y;
+                    float ratioDiff = value.y - m_Ratio.y;
+                    float finalSize = m_Size.y + sizeRatio * ratioDiff;
+                    Size = new Vector2(m_Size.x, finalSize);
+                }
+
+                m_Ratio = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets/Sets the screen size.
+        /// </summary>
+        public Vector2 Size
+        {
+            get { return m_Size; }
+            set
+            {
+                // If the X component of the size has changed
+                if (m_Size.x != value.x)
+                {
+                    m_Size.x = value.x;
+                    m_Size.y = m_Size.x / RatioFrac;
+                }
+                else
+                {
+                    m_Size.y = value.y;
+                    m_Size.x = m_Size.y * RatioFrac;
+                }
+
+                if(Camera != null)
+                {
+                    Camera.orthographicSize = m_Size.y / 2;
+                }
+            }
+        }
+
+        private float RatioFrac
+        {
+            get { return m_Ratio.x / m_Ratio.y; }
+        }
+
+        /// <summary>
+        /// Gets the defined camera, or the main camera if it's not assigned.
+        /// </summary>
+        public Camera Camera
+        {
+            get
+            {
+                if(m_Camera == null)
+                {
+                    m_Camera = Camera.main;
+                }
+                return m_Camera;
+            }
+            set
+            {
+                m_Camera = value;
+            }
+        }
+
+    }
+
+}
