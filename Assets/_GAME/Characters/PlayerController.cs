12f054cabec393ac5838db9c33d4ffa1fee0c929
Hubert GROSSIN <hubert.grossin@gmail.com>
2020-09-27 15:28:51 +0200

    Make demo movement & game scene

---
+++ Assets/_GAME/Characters/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+
+    ///<summary>
+    /// 
+    ///</summary>
+    //[AddComponentMenu("_GAME/PlayerController")]
+    public class PlayerController : MonoBehaviour
+    {
+
+        [Header("General Settings")]
+
+        [SerializeField, Range(1, 2)]
+        private int m_PlayerID = 1;
+
+        [Header("Movement")]
+
+        [SerializeField]
+        private float m_Speed = 6f;
+
+        private void Update()
+        {
+            Move(Time.deltaTime);
+        }
+
+        public void Move(float _DeltaTime)
+        {
+            float axis = 0f;
+
+            if (Input.GetKey(KeyCode.Q))
+            {
+                axis = -1f;
+            }
+            else if(Input.GetKey(KeyCode.D))
+            {
+                axis = 1f;
+            }
+
+            if(axis != 0f)
+            {
+                transform.position += Vector3.right * axis * m_Speed * _DeltaTime;
+            }
+        }
+
+    }
+
+}
